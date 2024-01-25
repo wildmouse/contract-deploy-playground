335c27dd46de2926e4ea893489c89919727cfce8
wildmouse <wildmouse0813@gmail.com>
2024-01-25 18:31:36 +0900

    Update config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
-import { HardhatUserConfig } from "hardhat/config";
+import {HardhatUserConfig} from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ledger";
+import {configDotenv} from "dotenv";
+
+configDotenv()
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
+    solidity: "0.8.19",
+    networks: {
+        sepolia: {
+            url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+        },
+    },
 };
 
 export default config;
