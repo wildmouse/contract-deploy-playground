20b16c9dde5c0e650e23a46f72569cc2ce0b9db9
wildmouse <wildmouse0813@gmail.com>
2024-01-25 18:35:24 +0900

    Add account config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,6 +10,7 @@ const config: HardhatUserConfig = {
     networks: {
         sepolia: {
             url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
+            accounts: [process.env.PRIVATE_KEY ?? ""],
         },
     },
 };
